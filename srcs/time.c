56a15087519c9dc8f8e65e77c7e142783cc9fc67
Ruben Dario Higuita Rubio <higuitaruben@hotmail.com>
2025-10-26 00:40:51 +0200

    Estructura del proyecto, archivo timr.c

---
+++ srcs/time.c
@@ -0,0 +1,24 @@
+#include "../philo.h"
+
+long	ft_atol(const char *str)
+{
+	long	num;
+	int		sign;
+
+	num = 0;
+	sign = 1;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-' || *str == '+')
+    {
+        if (*str == '-')
+            sign = -1;
+        str++;
+    }
+	while (*str >= '0' && *str <= '9')
+	{
+        num = num * 10 + (*str - '0');
+        str++;
+    }
+	return (num * sign);
+}
