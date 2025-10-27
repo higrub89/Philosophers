0e95577d99c9bfe88e8a56ed59a2acd3886884ce
Ruben Dario Higuita Rubio <higuitaruben@hotmail.com>
2025-10-27 18:52:47 +0100

    Tetsting proms argumentos check ok

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: ruben <ruben@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 17:32:50 by rhiguita          #+#    #+#             */
-/*   Updated: 2025/10/27 18:30:50 by ruben            ###   ########.fr       */
+/*   Updated: 2025/10/27 18:50:35 by ruben            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,16 @@ static int  check_args(t_sim *sim)
         return (display_error("time_to_eat debe ser > 0"), 0);
     if (sim->time_to_sleep <= 0)
         return (display_error("time_to_sleep debe ser > 0"), 0);
-    if (sim->num_meals_to_eat < 0)
-        return (display_error("num_meals_to_eat no puede ser negativo"), 0);
+    if (sim->num_meals_to_eat < -1 || sim->num_meals_to_eat == 0)
+        return (display_error("num_meals_to_eat no puede ser > 0 ó -1(infinito)"), 0);
     return (1);
 }
 
 static int   is_valid_number(const char *s)
 {
+    int digit;
+
+    digit = 0;
     while (*s == ' ' || (*s >= 9 && *s <= 13))
         s++;
     if (*s == '+' || *s == '-')
@@ -37,11 +40,13 @@ static int   is_valid_number(const char *s)
         return (0);
     while (*s)
     {
-        if (*s < '0' || *s > 9)
+        if (*s >= '0' || *s <= '9')
+            digit = 1;
+        else
             return (0);
         s++;
     }
-    return (1);
+    return (digit);
 }
 
 int parse_args(t_sim *sim, int ac, char **av)
@@ -57,9 +62,14 @@ int parse_args(t_sim *sim, int ac, char **av)
             return (display_error("Argumento contiene caracteres no validos"), 0);
         i++;
     }
+    sim->num_philos = (int)ft_atol(av[1]);
+    sim->time_to_die = ft_atol(av[2]);
+    sim->time_to_eat = ft_atol(av[3]);
+    sim->time_to_sleep = ft_atol(av[4]);
     if (ac == 6)
         sim->num_meals_to_eat = (int)ft_atol(av[5]);
     else
         sim->num_meals_to_eat = -1;
     return (check_args(sim));
+    
 }
